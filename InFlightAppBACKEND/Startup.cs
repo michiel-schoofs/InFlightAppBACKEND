14719794407bd5fb4d3fd8086f5b3d894baede4c
TyboVanderstraeten <vanderstraeten.tybo@hotmail.com>
2019-10-12 16:55:23 +0200

    Add dbinitializer

--- InFlightAppBACKEND/Startup.cs
+++ InFlightAppBACKEND/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InFlightAppBACKEND.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -30,12 +32,15 @@ namespace InFlightAppBACKEND
             #endregion
 
             #region DBContext
-
+            services.AddDbContext<DBContext>(options =>
+            {
+                options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]);
+            });
             #endregion
 
 
             #region Dependency Injections
-
+            services.AddScoped<DBInitializer>();
             #endregion
 
             #region NSwag
@@ -67,7 +72,7 @@ namespace InFlightAppBACKEND
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, DBInitializer dbInitializer)
         {
             if (env.IsDevelopment())
             {
@@ -88,6 +93,8 @@ namespace InFlightAppBACKEND
             app.UseOpenApi();
 
             app.UseCors("AllowAllOrigins");
+
+            dbInitializer.seedDatabase().Wait();
         }
     }
 }
