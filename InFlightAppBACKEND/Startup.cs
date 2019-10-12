09955d9dd8fb99110238c100189edf2d12c2e068
TyboVanderstraeten <vanderstraeten.tybo@hotmail.com>
2019-10-12 13:08:02 +0200

    Add dependencies

--- InFlightAppBACKEND/Startup.cs
+++ InFlightAppBACKEND/Startup.cs
@@ -25,7 +25,45 @@ namespace InFlightAppBACKEND
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            #region MVC
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            #endregion
+
+            #region DBContext
+
+            #endregion
+
+
+            #region Dependency Injections
+
+            #endregion
+
+            #region NSwag
+
+            #endregion
+
+            #region Default identity
+
+            #endregion
+
+            #region Authentication
+
+            #endregion
+
+            #region Authorization
+
+            #endregion
+
+            #region Identity
+
+            #endregion
+
+            #region CORS
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins", builder => builder.AllowAnyOrigin());
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,7 +79,9 @@ namespace InFlightAppBACKEND
             }
 
             app.UseHttpsRedirection();
+            app.UseAuthentication();
             app.UseMvc();
+            app.UseCors("AllowAllOrigins");
         }
     }
 }
