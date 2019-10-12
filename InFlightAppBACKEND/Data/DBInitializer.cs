19a809175707451b116a1c3c3853c46f614f58db
TyboVanderstraeten <vanderstraeten.tybo@hotmail.com>
2019-10-12 16:55:23 +0200

    Add dbinitializer

--- InFlightAppBACKEND/Data/DBInitializer.cs
+++ InFlightAppBACKEND/Data/DBInitializer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,22 @@ namespace InFlightAppBACKEND.Data
 {
     public class DBInitializer
     {
+        private DBContext _dbContext;
+        private UserManager<IdentityUser> _userManager;
+
+        public DBInitializer(DBContext dbContext, UserManager<IdentityUser> userManager)
+        {
+            _dbContext = dbContext;
+            _userManager = userManager;
+        }
+
+        public async Task seedDatabase()
+        {
+            _dbContext.Database.EnsureDeleted();
+            if (_dbContext.Database.EnsureCreated())
+            {
+
+            }
+        }
     }
 }
