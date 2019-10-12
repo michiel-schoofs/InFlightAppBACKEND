6f846e9f861cb9b38bb459b8d488dfbbed467d9d
TyboVanderstraeten <vanderstraeten.tybo@hotmail.com>
2019-10-12 15:21:08 +0200

    Add dbcontext

---
+++ InFlightAppBACKEND/Data/DBContext.cs
@@ -0,0 +1,40 @@
+﻿using InFlightAppBACKEND.Models.Domain;
+using InFlightAppBACKEND.Data.Mappers;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace InFlightAppBACKEND.Data
+{
+    public class DBContext : IdentityDbContext
+    {
+        public DBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new ConversationConfiguration());
+            builder.ApplyConfiguration(new CrewMemberConfiguration());
+            builder.ApplyConfiguration(new FlightConfiguration());
+            builder.ApplyConfiguration(new MessageConfiguration());
+            builder.ApplyConfiguration(new OrderConfiguration());
+            builder.ApplyConfiguration(new OrderLineConfiguration());
+            builder.ApplyConfiguration(new PassengerConfiguration());
+            builder.ApplyConfiguration(new PassengerConversationConfiguration());
+            builder.ApplyConfiguration(new ProductConfiguration());
+            builder.ApplyConfiguration(new ProductTypeConfiguration());
+            builder.ApplyConfiguration(new SeatConfiguration());
+            builder.ApplyConfiguration(new SeatTypeConfiguration());
+            builder.ApplyConfiguration(new TravelGroupConfiguration());
+        }
+
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Seat> Seats { get; set; }
+        public DbSet<Passenger> Passengers { get; set; }
+        public DbSet<CrewMember> CrewMembers { get; set; }
+        public DbSet<Conversation> Conversations { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
