ff07ca33ab7f52199f6438bd2922bdaa5d331df8
TyboVanderstraeten <vanderstraeten.tybo@hotmail.com>
2019-10-12 15:39:23 +0200

    Add half of the configurational mappers

---
+++ InFlightAppBACKEND/Data/Mappers/CrewMemberConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using InFlightAppBACKEND.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InFlightAppBACKEND.Data.Mappers
+{
+    public class CrewMemberConfiguration : IEntityTypeConfiguration<CrewMember>
+    {
+        public void Configure(EntityTypeBuilder<CrewMember> builder)
+        {
+            builder.ToTable("CrewMember");
+            builder.HasKey(c => c.UserId);
+            builder.Property(c => c.FirstName).IsRequired();
+            builder.Property(c => c.LastName).IsRequired();
+        }
+    }
+}
---
+++ InFlightAppBACKEND/Data/Mappers/PassengerConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using InFlightAppBACKEND.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InFlightAppBACKEND.Data.Mappers
+{
+    public class PassengerConfiguration : IEntityTypeConfiguration<Passenger>
+    {
+        public void Configure(EntityTypeBuilder<Passenger> builder)
+        {
+            builder.ToTable("Passenger");
+            builder.HasKey(p => p.UserId);
+            builder.Property(p => p.FirstName).IsRequired();
+            builder.Property(p => p.LastName).IsRequired();
+            builder.Property(p => p.TravelGroup).IsRequired(false);
+            builder.HasMany(p => p.Orders)
+                .WithOne()
+                .IsRequired()
+                .HasForeignKey(c => c.PassengerId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
