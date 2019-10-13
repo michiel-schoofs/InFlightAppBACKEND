b9173c735712a9b793241e5c33e51ba16088913e
TyboVanderstraeten <vanderstraeten.tybo@hotmail.com>
2019-10-13 14:47:12 +0200

    Add product and order concrete repository

--- InFlightAppBACKEND/Data/Repositories/OrderRepository.cs
+++ InFlightAppBACKEND/Data/Repositories/OrderRepository.cs
@@ -2,10 +2,60 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InFlightAppBACKEND.Models.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace InFlightAppBACKEND.Data.Repositories
 {
-    public class OrderRepository
+    public class OrderRepository : IOrderRepository
     {
+        private DbSet<Order> _orders;
+        private DBContext _dbContext;
+
+        public OrderRepository(DBContext dbContext)
+        {
+            _orders = dbContext.Orders;
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<Order> GetAll()
+        {
+            return _orders
+                .Include(o => o.OrderLines)
+                .ThenInclude(ol => ol.Product)
+                .ToList();
+        }
+
+        public IEnumerable<Order> GetAllUnprocessed()
+        {
+            return _orders
+                .Where(o => !o.IsDone)
+                .Include(o => o.OrderLines)
+                .ThenInclude(ol => ol.Product)
+                .ToList();
+        }
+
+        public Order GetById(int id)
+        {
+            return _orders
+                .Include(o => o.OrderLines)
+                .ThenInclude(ol => ol.Product)
+                .SingleOrDefault(o => o.OrderId == id);
+        }
+
+        public void Add(Order order)
+        {
+            _orders.Add(order);
+        }
+
+        public void Remove(Order order)
+        {
+            _orders.Remove(order);
+        }
+
+        public void SaveChanges()
+        {
+            _dbContext.SaveChanges();
+        }
     }
 }
--- InFlightAppBACKEND/Data/Repositories/ProductRepository.cs
+++ InFlightAppBACKEND/Data/Repositories/ProductRepository.cs
@@ -1,11 +1,47 @@
-﻿using System;
+﻿using InFlightAppBACKEND.Data.Mappers;
+using InFlightAppBACKEND.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace InFlightAppBACKEND.Data.Repositories
 {
-    public class ProductRepository
+    public class ProductRepository : IProductRepository
     {
+        private DbSet<Product> _products;
+        private DBContext _dbContext;
+
+        public ProductRepository(DBContext dbContext)
+        {
+            _products = dbContext.Products;
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _products.Include(p => p.Type).ToList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _products.Include(p => p.Type).SingleOrDefault(p => p.ProductId == id);
+        }
+
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Remove(Product product)
+        {
+            _products.Remove(product);
+        }
+
+        public void SaveChanges()
+        {
+            _dbContext.SaveChanges();
+        }
     }
 }
