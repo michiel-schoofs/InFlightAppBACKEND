b8acdf72df2d5895d84e073922c5bdcad2c3e09a
TyboVanderstraeten <vanderstraeten.tybo@hotmail.com>
2019-10-13 14:40:20 +0200

    Add crewmember and flight concrete repository

---
+++ InFlightAppBACKEND/Data/Repositories/FlightRepository.cs
@@ -0,0 +1,53 @@
+﻿using InFlightAppBACKEND.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InFlightAppBACKEND.Data.Repositories
+{
+    public class FlightRepository : IFlightRepository
+    {
+        private DbSet<Flight> _flights;
+        private DBContext _dbContext;
+
+        public FlightRepository(DBContext dbContext)
+        {
+            _flights = dbContext.Flights;
+            _dbContext = dbContext;
+        }
+
+
+
+        public IEnumerable<Flight> GetAll()
+        {
+            return _flights.ToList();
+        }
+
+        public IEnumerable<Passenger> GetPassengersFromFlight(int id)
+        {
+            return _flights.SingleOrDefault(f => f.FlightId == id).Seats.Select(s => s.Passenger).ToList();
+        }
+
+        public Flight GetById(int id)
+        {
+            return _flights.SingleOrDefault(f => f.FlightId == id);
+        }
+
+        public void Add(Flight flight)
+        {
+            _flights.Add(flight);
+        }
+
+        public void Remove(Flight flight)
+        {
+            _flights.Remove(flight);
+        }
+
+        public void SaveChanges()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
